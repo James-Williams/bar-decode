1a6b23725b59513ba9e92b381d5637fd7a6a4535
j <jamie.williams89@gmail.com>
2015-11-16 23:11:55 +0000

    Working decode of left group from IMAGE!!

--- src/barcode_noob/image.clj
+++ src/barcode_noob/image.clj
@@ -1,6 +1,7 @@
 (ns barcode-noob.image
   (:gen-class)
   (:require [clojure.string :as str])
+  (:use barcode-noob.core)
   )
 
 (def threshold-delta 0.42)
@@ -18,6 +19,53 @@
 (defn compress [s]
   (->> (partition-by identity s) (map (juxt count first))))
 
+(defn scale-list [xs]
+  (let [ sum (reduce + xs) ]
+    (map #(/ % sum) xs)))
+
+(defn process-bits [xs]
+  (let [ compressed (compress xs) ]
+    (assert (= (count compressed) 4))
+    (->>  compressed
+          (map first)
+          scale-list
+      )))
+
+(defn abs [v] (max v (- v)))
+
+(defn dist
+  [l1 l2]
+  (assert (= (count l1) (count l2)))
+  (let [ diffs (map #(abs (- %1 %2)) l1 l2)
+       ]
+    (reduce + diffs)))
+
+(defn match-left-digits
+  [scaled-run]
+  (assert (= (count scaled-run) 4))
+  (assert (= (reduce + scaled-run) 1))
+  (let [ scaled-odd  (map process-bits left-odd-bits)
+         scaled-even (map process-bits left-even-bits)
+         scaled-digits (concat scaled-odd scaled-even)
+         dist-vec (map #(dist scaled-run %) scaled-digits)
+        ]
+    (->> dist-vec
+         (map vector (range))
+         (sort-by #(nth % 1))
+         (map first)
+         (take 3)
+         (map #(mod % 10)) ;; TODO Need to include odd/even in output
+    )))
+
+(defn process-row
+  [row]
+  (let [
+         runs   (map first (compress row))
+         digit-runs (drop 4 runs)
+       ]
+    digit-runs))
+
+
 (defn process-pgm-lines
   [delta ls]
   (let [
@@ -31,14 +79,19 @@
          rows   (partition width values)
          row    (nth rows 21)
          row-bits (grey-threshold delta row)
-         runs   (map first (compress row-bits))
        ]
     (assert (= type "P2"))
     (assert (= (count rows) height))
-    (list runs)))
+    (list (process-row row-bits))))
 
 (defn read-pgm
   [filename]
   (with-open [rdr (clojure.java.io/reader filename)]
     (doall (process-pgm-lines threshold-delta (line-seq rdr)))))
+
+;; TODO Test code from REPL - WORKING DECODE OF LEFT DIGITS!!!
+;;        Need to build this into more functions!
+
+(def l (first (read-pgm "barcode-example1.pgm")))
+(def working (map match-left-digits (map scale-list (take 6 (partition 4 l)))))
 
