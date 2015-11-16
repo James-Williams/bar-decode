eda8051fbbac28596674723f8bd2d51fcf2946bc
j <jamie.williams89@gmail.com>
2015-11-16 17:57:37 +0000

    Added enc-barcode function

--- src/barcode_noob/core.clj
+++ src/barcode_noob/core.clj
@@ -73,4 +73,21 @@
   "given a seq of digits, generate a seq of bits for the right group"
   [ds]
   (assert (= (count ds) 6))
-    (map #(get right-bits %) ds))
+  (map #(get right-bits %) ds))
+
+(defn enc-barcode
+  "given a seq of 12 digits, generate a seq of bits for the entire barcode"
+  [d-in]
+  (assert (= (count d-in) 12))
+  (let [
+         last-digit (check-digit d-in)
+         ds           (concat d-in (list last-digit))
+         start-bits   (list '(1 0 1))
+         end-bits     start-bits
+         center-bits  (list '(0 1 0 1 0))
+         first-digit  (first ds)
+         left-digits  (take 6 (drop 1 ds))
+         right-digits (drop 7 ds)
+         left-bits    (enc-left-group first-digit left-digits)
+         right-bits   (enc-right-group right-digits)]
+    (apply concat (concat start-bits left-bits center-bits right-bits end-bits))))
