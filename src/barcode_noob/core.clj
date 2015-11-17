33007a8afae7b0e36c105cb984a0c2ec2926be7b
j <jamie.williams89@gmail.com>
2015-11-17 20:21:08 +0000

    Added command line access to barcode decoding

--- src/barcode_noob/core.clj
+++ src/barcode_noob/core.clj
@@ -6,12 +6,19 @@
 (def THRESHOLD-DELTA 0.42)
 
 (defn process-pgm-lines
-  [delta ls]
+  [ls]
   (let [ rows (pgm-to-pixels ls)
-         rows-bits (map #(grey-threshold delta %) rows)
+         rows-bits (map #(grey-threshold THRESHOLD-DELTA %) rows)
        ]
     (reduce concat (map process-row rows-bits))))
 
 (defn scan-pgm [filename] 
-  (process-pgm #(process-pgm-lines THRESHOLD-DELTA %) filename))
+  (process-pgm process-pgm-lines filename))
 
+(defn -main [& args]
+  (if (not= (count args) 1)
+    (println "Usage: lein run <pgm-filename>")
+    (->> (first args)
+         scan-pgm
+         first
+         println)))
